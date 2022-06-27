2254e8534e6f2d0a575de88f18dce4622e499f75
bvinayratna <bezzam.vinay@gmail.com>
2022-06-27 10:37:31 +0530

    added loginpage.xhtml with JSF_face_Validation

---
+++ src/main/java/jsfvalidation/userData.java
@@ -0,0 +1,40 @@
+package jsfvalidation;
+
+import javax.faces.bean.ManagedBean;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+
+@ManagedBean(name="user",eager=true)
+
+public class userData {
+
+	@Size(min=1)
+    String name;
+	@Min(18)
+	@Max(30)
+	int age;
+	@Size(min=10,max=10)
+	
+
+	String mobileno;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getMobileno() {
+		return mobileno;
+	}
+	public void setMobileno(String mobileno) {
+		this.mobileno = mobileno;
+	}
+}
